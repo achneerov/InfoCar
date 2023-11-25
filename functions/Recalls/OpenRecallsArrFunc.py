fe81bbefa507738e2272100fe8a72e20950ec389
achneerov <achneerov@gmail.com>
2023-11-25 15:39:20 -0500

    better

--- functions/Recalls/OpenRecallsArrFunc.py
+++ functions/Recalls/OpenRecallsArrFunc.py
@@ -1,14 +1,14 @@
-from functions.Recalls.HyundaiRecall import HyundaiRecallFunc
-from functions.Recalls.HyundaiRecall import HyundaiRecallFuncTest
+from functions.Recalls.HyundaiRecall import hyundai_recall_func
+from functions.Recalls.HyundaiRecall import hyundai_recall_func_test
 
 
 def open_recalls_arr(VIN, make):
     make = make.lower()
 
     if make == "hyundai":
-        return HyundaiRecallFunc(VIN)
+        return hyundai_recall_func(VIN)
 
 def open_recalls_arr_test(VIN, make):
     make = make.lower()
     if make == "hyundai":
-        return HyundaiRecallFuncTest(VIN)
+        return hyundai_recall_func_test(VIN)
--- functions/StolenVehicleArrFunc.py
+++ functions/StolenVehicleArrFunc.py
@@ -1,11 +1,12 @@
-
-
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
+import time
+
+def stolen_vehicle_arr_test(VIN):
+    return ['No records were found as of: November 25, 2023']
 
 def stolen_vehicle_arr(VIN):
     StolenArr = []
@@ -20,56 +21,38 @@ def stolen_vehicle_arr(VIN):
         # Load the website
         driver.get(url)
 
-        # Handle cookies popup
-        try:
-            # Wait for the "Accept cookies" button to be clickable
-            cookies_button = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CLASS_NAME, 'accept-cookies')))
-            cookies_button.click()
-        except Exception as e:
-            print(f"Error handling cookies popup: {str(e)}")
-
-        # Handle region/language popup
-        try:
-            region_popup = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CLASS_NAME, 'province-selector-form-container-formelements-submit')))
-            region_popup.click()
-        except Exception as e:
-            print(f"Error handling region/language popup: {str(e)}")
-
         # Find the input field with the specific class and id
-        vin_input = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'recallsVIN')))
+        vin_input = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'searchItem')))
+        time.sleep(2)  # Add a sleep interval
 
         # Set the VIN in the input field
         vin_input.send_keys(VIN)
 
-        # Locate the form and submit it
-        submit_button = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CLASS_NAME, 'op-cta')))
+
+
+        # Find the checkbox element by ID
+        checkbox = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'tou')))
+        time.sleep(2)  # Add a sleep interval
+
+        # Check the checkbox
+        checkbox.click()
+
+        time.sleep(2)
+
+        submit_button = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'search')))
         submit_button.click()
 
-        # Wait for the recalls section to load
-        recalls_section = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, 'op-safety-recalls')))
+        time.sleep(2)
 
-        # Parse the HTML content
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-        # recall_codes = soup.select('.op-safety-recalls__cell-value recall-number')
-        recall_codes = soup.select('.op-safety-recalls__cell.recall-number')
-        recall_titles = soup.select('.op-safety-recalls__accordion-content-heading')
-        recall_descs = soup.select('.op-safety-recalls__accordion-content-description')
+        results_message = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'rsltsMsg')))
 
-        if recall_codes:
-            for i in range(len(recall_codes)):
-                StolenArr.append([recall_codes[i].text.strip(), recall_titles[i].text.strip(), recall_descs[i].text.strip()])
-        else:
-            print("None")
-            StolenArr.append("The car is not actively stolen.")
+        StolenArr.append(results_message.text)
 
     except Exception as e:
         print(f"An error occurred: {str(e)}")
 
     finally:
-        # Close the browser
         driver.quit()
 
     return StolenArr
-
-
 
